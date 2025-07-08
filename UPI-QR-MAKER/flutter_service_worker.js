20bca1fec7335643fceffa7171c8a74208a8e675
Rohan Batra <rohanbatra.in@gmail.com>
2025-07-08 07:25:43 +0000

    🚀 Deploy Flutter Web build to GitHub Pages

--- UPI-QR-MAKER/flutter_service_worker.js
+++ UPI-QR-MAKER/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "icons/Icon-maskable-192.png": "8653118bab907f60d1fe7bb459544a0b",
 "icons/Icon-192.png": "8653118bab907f60d1fe7bb459544a0b",
 "icons/Icon-maskable-512.png": "f96aaef6108fddebb4db05342d96d1f9",
-"flutter_bootstrap.js": "6d2945aa89b73ab3ba87ef18f61f950b",
+"flutter_bootstrap.js": "73c1a7a0700862679c522406d5be816d",
 "version.json": "bcf3ac4b1a729ab1277c13f9ceb7e5ec"};
 // The application shell files that are downloaded before a service worker can
 // start.
