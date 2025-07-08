2a52f202b0e77b17bc793dd2d112d46e44f5539e
Rohan Batra <rohanbatra.in@gmail.com>
2025-07-08 23:37:23 +0000

    🚀 Deploy Flutter Web build to GitHub Pages

--- UPI-QR-MAKER/flutter_bootstrap.js
+++ UPI-QR-MAKER/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "786110796"
+    serviceWorkerVersion: "3998934924"
   }
 });
